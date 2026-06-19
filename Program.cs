c21e49a120fcd20c85262746c6dff4bdf07e1990
Sbomelo <s225118440@mandela.ac.za>
2026-06-19 17:30:14 +0200

    Registered the background service for checking over due tasks

--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Service Registration
+// SERVICE REGISTRATION
 
-//Register TaskStore as a singleton - one instance for the app lifetime
-//Every hub method invocation and background service gets the SAME instance
+//Register TaskStore
 builder.Services.AddSingleton<TaskStore>();
+
+//Overdue Task checker
+builder.Services.AddHostedService<OverdueTaskChecker>();
 
 //Register SignalR with JSON enum string serialization configured
 builder.Services.AddSignalR()
